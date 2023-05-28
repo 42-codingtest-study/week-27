ae184ed5b185d2536f10d1f874b7f8371c5df243
sally3275 <sally3275@naver.com>
2023-05-28 23:56:52 +0900

    a

---
+++ b2_13300/Main.java
@@ -0,0 +1,17 @@
+package b2_13300;
+import java.util.*;
+/*
+ * 고민중..
+ * 학년별 성별 구분을 배열 / 해시맵 중에 뭐로 저장할지
+ * 방 수 추가를 매번 계산해서 학년별 명수를 초기화할지 / 마지막에 한꺼번에 할지
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt(), k = s.nextInt();
+		
+	}
+
+}
